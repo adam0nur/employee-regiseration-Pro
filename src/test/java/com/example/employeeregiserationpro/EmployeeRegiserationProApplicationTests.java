a58dd627bd0cd5acb7fb7e166e2a615d5e4d0364
adam0nur <adamnur0117@gmail.com>
2022-03-23 21:59:49 -0500

    Initial commit

---
+++ src/test/java/com/example/employeeregiserationpro/EmployeeRegiserationProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeregiserationpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegiserationProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
